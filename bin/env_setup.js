b2e1481967b65ee732e67cc4a83a6b4904bd7152
Atharv Naphade <cloversoftinc@outlook.com>
2021-12-05 00:17:43 +0530

    bug fixing in progress

--- bin/env_setup.js
+++ bin/env_setup.js
@@ -2,13 +2,18 @@ import puppeteer from 'puppeteer-core';
 import chromeLauncher from 'chrome-launcher';
 
 export default async function env_setup() {
-    const chrome = await chromeLauncher.launch();
-    const browser = await puppeteer.connect({ 
-        browserURL: `http://127.0.0.1:${chrome.port}`,  ///// CHANGE THE PORT NUMBER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-        defaultViewport: null
-    });
-    const main_page = await browser.newPage();
-    await main_page.goto('https://uonline.newcastle.edu.au/');
-
-    return { "main_page": main_page, "browser": browser };
+    try{
+        const chrome = await chromeLauncher.launch();
+        const browser = await puppeteer.connect({ 
+            browserURL: `http://127.0.0.1:${chrome.port}`,  ///// CHANGE THE PORT NUMBER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            defaultViewport: null
+        });
+        const main_page = await browser.newPage();
+        await main_page.goto('https://uonline.newcastle.edu.au/');
+    
+        return { "main_page": main_page, "browser": browser };
+    }
+    catch{
+        console.log("\nAN EXPECTED ERROR OCCURED.");
+    }
 };
---
+++ bin/prod
@@ -0,0 +1,5 @@
+require('babel-register')
+require("babel-core/register");
+require("babel-polyfill"); // If you use async and await
+// This us where our main application index path is
+require('../src/index');
